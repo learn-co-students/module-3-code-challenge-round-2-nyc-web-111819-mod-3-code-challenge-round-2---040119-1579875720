7f97cad6a85a321cdbe900703eb16e04586adf78
kevinkim1030 <kevin.kim1030@gmail.com>
2020-01-24 10:48:22 -0500

    solution with created elements

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 document.addEventListener('DOMContentLoaded', (event) => {
+  console.log('DOM loaded!')
   const baseUrl = 'http://localhost:3000/beers'
   let beerUl = document.getElementById('list-group')
   let beerDetailDiv = document.getElementById('beer-detail')
   let textarea = document.getElementById('description')
   
-  
-  console.log('DOM loaded!')
+function fetchBeers(){
   fetch(baseUrl)
   .then(resp => resp.json())
   .then(beers => {
@@ -17,6 +17,8 @@ document.addEventListener('DOMContentLoaded', (event) => {
       beerUl.appendChild(beerLi)
     })
   })//end of beers fetch 
+}
+fetchBeers()
 
 beerUl.addEventListener('click', (event) => {
   // console.log(event.target)
@@ -24,53 +26,77 @@ beerUl.addEventListener('click', (event) => {
   fetch(`http://localhost:3000/beers/${event.target.id}`)
   .then(resp => resp.json())
   .then(beer => {
-    let beerDiv = document.createElement('div')
-    // beerDiv.id = beer.id
-    beerDiv.innerHTML = `
-      <h1>${beer.name}</h1>
-      <img src=${beer.image_url}>
-      <h3>${beer.tagline}</h3>
-      <textarea>${beer.description}</textarea>
-      <button id=${beer.id} class="btn btn-info">
-        Save
-      </button>
-    `
-    beerDetailDiv.appendChild(beerDiv)
+    createBeerDiv(beer)
   })//end of beer detail fetch
 })
 
+function createBeerDiv(beer){
+  let beerDiv = document.createElement('div')
+  // first attempt route using innerHTML
+  // beerDiv.id = beer.id
+  // beerDiv.innerHTML = `
+  //   <h1>${beer.name}</h1>
+  //   <img src=${beer.image_url}>
+  //   <h3>${beer.tagline}</h3>
+  //   <textarea>${beer.description}</textarea>
+  //   <button id=${beer.id} class="btn btn-info">
+  //     Save
+  //   </button>
+  // `
+
+  let name = document.createElement('H1')
+  let img = document.createElement('IMG')
+  let tagLine = document.createElement('H3')
+  let beerTextArea = document.createElement('TEXTAREA')
+  let saveButton = document.createElement('BUTTON')
+    name.textContent = `${beer.name}`
+    img.src = `${beer.image_url}`
+    tagLine.textContent = `${beer.tagline}`
+    beerTextArea.textContent = `${beer.description}`
+    saveButton.id = `${beer.id}`
+    saveButton.className = "btn btn-info"
+    saveButton.textContent = "Save"
+      beerDiv.appendChild(name)
+      beerDiv.appendChild(img)
+      beerDiv.appendChild(tagLine)
+      beerDiv.appendChild(beerTextArea)
+      beerDiv.appendChild(saveButton)
+
+  beerDetailDiv.appendChild(beerDiv)
+}
+
 beerDetailDiv.addEventListener('click', (event) => {
-  // console.log(event.target)
-  // event.preventDefault()
-  // console.log(event.target.textContent = "Save")
+  //first attempt route using innerHTML
+
+  // if (event.target.className === "btn btn-info"){
+    //   console.log('clicked button!')
+    //   console.log(event.target.parentNode.childNodes[7].value)
+    //   let newDescription = event.target.parentNode.childNodes[7].value
+    
+    //   fetch(`http://localhost:3000/beers/${event.target.id}`, {
+      //     method: 'PATCH',
+      //     headers: {
+        //       'content-type': 'application/json',
+        //       accepts: 'application/json'
+        //     },
+        //     body: JSON.stringify({description: newDescription})
+        //   })
+        // }
+        
   if (event.target.className === "btn btn-info"){
     console.log('clicked button!')
-    let newDescription = event.target.parentNode.childNodes[7].value
-    // console.log(event.target.parentNode.childNodes[7].value)
-  
+    console.log(event.target.parentNode.childNodes[3].value)
+    let newDescription = event.target.parentNode.childNodes[3].value
 
-
-  fetch(`http://localhost:3000/beers/${event.target.id}`, {
-    method: 'PATCH',
-    headers: {
-      'content-type': 'application/json',
-      accepts: 'application/json'
-    },
-    body: JSON.stringify({
-      description: newDescription
-    })
-  })
+    fetch(`http://localhost:3000/beers/${event.target.id}`, {
+      method: 'PATCH',
+      headers: {
+          'content-type': 'application/json',
+          accepts: 'application/json'
+        },
+        body: JSON.stringify({description: newDescription})
+      })
   }
+
 })
-
-
-
-
-
-
-
-
-
-
-
 })//end of dom content event listener
